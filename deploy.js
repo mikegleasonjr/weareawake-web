a72447191d6f023faf02d9fd1908f57c69c7e4db
Mike Gleason jr Couturier <mikegleasonjr@gmail.com>
2013-12-05 17:22:20 -0500

    Bookmarking generates a 500 error when pushing event on Facebook

--- deploy.js
+++ deploy.js
@@ -21,18 +21,17 @@ task('web', 'Deploys website', function (controller, archivePath) {
   var archiveFilename = path.basename(archivePath),
     remoteDeployDir = '/srv/weareawake-web',
     remoteArchivePath = path.join(remoteDeployDir, archiveFilename),
-    unpackedAppDirectoryName = path.basename(remoteArchivePath, '.tar.gz')
-    remoteAppDir = path.join(remoteDeployDir, unpackedAppDirectoryName);
-
-  var sshTasks = [
-    'tar zxvf ' + remoteArchivePath + ' -C ' + remoteDeployDir,
-    'rm ' + remoteArchivePath,
-    'cd ' + remoteAppDir + ' && npm install --production',
-    '/sbin/stop weareawake-web || true',
-    'rm /srv/weareawake-web/current || true',
-    'ln -s ' + remoteAppDir + ' /srv/weareawake-web/current',
-    '/sbin/start weareawake-web'
-  ];
+    unpackedAppDirectoryName = path.basename(remoteArchivePath, '.tar.gz'),
+    remoteAppDir = path.join(remoteDeployDir, unpackedAppDirectoryName),
+    sshTasks = [
+      'tar zxvf ' + remoteArchivePath + ' -C ' + remoteDeployDir,
+      'rm ' + remoteArchivePath,
+      'cd ' + remoteAppDir + ' && npm install --production',
+      '/sbin/stop weareawake-web || true',
+      'rm /srv/weareawake-web/current || true',
+      'ln -s ' + remoteAppDir + ' /srv/weareawake-web/current',
+      '/sbin/start weareawake-web'
+    ];
 
   controller.scp(archivePath, remoteDeployDir, function next() {
     if (sshTasks.length) {
