320a426ab42721c3cb1dc4689e9461b4c72beb8d
Mike Gleason jr Couturier <mikegleasonjr@gmail.com>
2014-01-31 16:32:02 -0500

    Rewrite of deploy script: separated the deploy in 2 pools

--- deploy.js
+++ deploy.js
@@ -1,57 +1,128 @@
 #!/usr/local/bin/node
 
-var control = require('control'),
-  task = control.task,
-  path = require('path');
-
-task('prod', 'Defines production environment', function () {
-  var config = {
-    'web01': { user: 'root' },
-    'web02': { user: 'root' }
+var path = require('path'),
+  fs = require('fs'),
+  control = require('control'),
+  async = require('async'),
+  environments = {
+    prod: {
+      loadbalancer: { lb01: { user: 'root' }},
+      webpool1: { web01: { user: 'root' }},
+      webpool2: { web02: { user: 'root' }}
+    }
   };
 
-  return control.controllers(config);
-});
+function deploy(lb, webpool1, webpool2, archive) {
+  async.series([
+    // deploy pool1
+    function(next) { changeLBServerState('disable', lb, webpool1, next); },
+    function(next) { waitSeconds(10, next); },
+    function(next) { deployPool(webpool1, archive, next); },
+    function(next) { changeLBServerState('enable', lb, webpool1, next); },
+    // deploy pool2
+    function(next) { changeLBServerState('disable', lb, webpool2, next); },
+    function(next) { waitSeconds(10, next); },
+    function(next) { deployPool(webpool2, archive, next); },
+    function(next) { changeLBServerState('enable', lb, webpool2, next); },
+  ], function(err) {
+    console.log(err ? 'ERROR: ' + err : 'DONE!');
+  });
+}
 
-task('web', 'Deploys website', function (controller, archivePath) {
-  if (!archivePath) {
-    console.error('please specify the archive to deploy from archives/*');
+function changeLBServerState(action, lb, webpool, next) {
+  async.eachSeries(webpool, function(server, next) {
+    lb.ssh('echo "' + action + ' server http00/' + server.address +'.weareawake.net" | socat stdio /var/lib/haproxy/stats');
+  }, next);
+}
+
+function waitSeconds(count, next) {
+  setTimeout(next, count * 1000);
+}
+
+function deployPool(webpool, archive, next) {
+  async.each(webpool, function(server, nextServer){
+    var archiveFilename = path.basename(archive),
+      remoteDeployDir = '/srv/weareawake-web',
+      remoteArchivePath = path.join(remoteDeployDir, archiveFilename),
+      unpackedAppDirectoryName = path.basename(remoteArchivePath, '.tar.gz'),
+      remoteAppDir = path.join(remoteDeployDir, unpackedAppDirectoryName),
+      sshTasks = [
+        'tar zxvf ' + remoteArchivePath + ' -C ' + remoteDeployDir,
+        'rm ' + remoteArchivePath,
+        'cd ' + remoteAppDir + ' && npm install --production',
+        'initctl stop weareawake-web || true',
+        'rm /srv/weareawake-web/current || true',
+        'ln -s ' + remoteAppDir + ' /srv/weareawake-web/current',
+        'initctl start weareawake-web'
+      ];
+
+    server.scp(archive, remoteDeployDir, function nextSsh() {
+      server.ssh(sshTasks.shift(), sshTasks.length ? nextSsh : nextServer);
+    });
+  }, function() {
+    next();
+  });
+}
+
+(function(env, archive) {
+  if (env !== 'prod') {
+    console.error('please specify a valid environment. Choices are: [prod]');
     return;
   }
 
-  var archiveFilename = path.basename(archivePath),
-    remoteDeployDir = '/srv/weareawake-web',
-    remoteArchivePath = path.join(remoteDeployDir, archiveFilename),
-    unpackedAppDirectoryName = path.basename(remoteArchivePath, '.tar.gz'),
-    remoteAppDir = path.join(remoteDeployDir, unpackedAppDirectoryName),
-    sshTasks = [
-      'tar zxvf ' + remoteArchivePath + ' -C ' + remoteDeployDir,
-      'rm ' + remoteArchivePath,
-      'cd ' + remoteAppDir + ' && npm install --production',
-      'initctl stop weareawake-web || true',
-      'rm /srv/weareawake-web/current || true',
-      'ln -s ' + remoteAppDir + ' /srv/weareawake-web/current',
-      'initctl start weareawake-web'
-    ];
+  if (!fs.existsSync(archive)) {
+    console.error('please specify a valid archive to deploy');
+    return;
+  }
 
-  controller.scp(archivePath, remoteDeployDir, function next() {
-    if (sshTasks.length) {
-      controller.ssh(sshTasks.shift(), next);
-    }
-  });
-});
+  var lb = control.controllers(environments[env].loadbalancer)[0],
+    webpool1 = control.controllers(environments[env].webpool1),
+    webpool2 = control.controllers(environments[env].webpool2);
 
-task('status', 'Reports web status', function (controller) {
-  var sshTasks = [
-    'initctl status weareawake-web',
-    'service nginx status'
-  ];
+  deploy(lb, webpool1, webpool2, archive);
+}).apply(this, process.argv.splice(2));
 
-  controller.ssh(sshTasks.shift(), function next() {
-    if (sshTasks.length) {
-      controller.ssh(sshTasks.shift(), next);
-    }
-  });
-});
+/*
+ task('web', 'Deploys website', function (controller, archivePath) {
+ if (!archivePath) {
+ console.error('please specify the archive to deploy from archives*/
+/*');
+ return;
+ }
 
-control.begin();
+ var archiveFilename = path.basename(archivePath),
+ remoteDeployDir = '/srv/weareawake-web',
+ remoteArchivePath = path.join(remoteDeployDir, archiveFilename),
+ unpackedAppDirectoryName = path.basename(remoteArchivePath, '.tar.gz'),
+ remoteAppDir = path.join(remoteDeployDir, unpackedAppDirectoryName),
+ sshTasks = [
+ 'tar zxvf ' + remoteArchivePath + ' -C ' + remoteDeployDir,
+ 'rm ' + remoteArchivePath,
+ 'cd ' + remoteAppDir + ' && npm install --production',
+ 'initctl stop weareawake-web || true',
+ 'rm /srv/weareawake-web/current || true',
+ 'ln -s ' + remoteAppDir + ' /srv/weareawake-web/current',
+ 'initctl start weareawake-web'
+ ];
+
+ controller.scp(archivePath, remoteDeployDir, function next() {
+ if (sshTasks.length) {
+ controller.ssh(sshTasks.shift(), next);
+ }
+ });
+ });
+
+ task('status', 'Reports web status', function (controller) {
+ var sshTasks = [
+ 'initctl status weareawake-web',
+ 'service nginx status'
+ ];
+
+ controller.ssh(sshTasks.shift(), function next() {
+ if (sshTasks.length) {
+ controller.ssh(sshTasks.shift(), next);
+ }
+ });
+ });
+ */
+
